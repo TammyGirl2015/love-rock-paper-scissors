7fa284d0aeec02c892515f9b7553be2c3be04663
TammyGirl2015 <mtamarie@yahoo.com>
2024-03-26 11:28:34 +0000

    add while loop to determine winner of round

--- run.py
+++ run.py
@@ -1,16 +1,30 @@
 import random
 
-#initialise scores
+# Initialise scores
 player_score = 0
 computer_score = 0
 
-#Welcome message and instructions on how to play
+# Welcome message and instructions
 print("Welcome to rock, paper, scissors.")
-print("To play, the user should choose enter their choice of either rock, paper or scissors in the terminal in lowercase.")
-print("The computer will also choose one of the three choices. ")
-print("The rules to win are as follows: \nrock smashes scissors, \nscissors cuts paper and \npaper covers rock.")
-print("The terminal will display the winner by writing the score: \n1 for the winner and \n0 for the loser at the end of each round.")
+print("To play, enter your choice of rock, paper, or scissors in lowercase.")
+print("The computer will also choose one of the three choices.")
+print("Rock smashes scissors, scissors cuts paper, and paper covers rock.")
 print("The game series ends when either the user or computer wins three rounds first.")
-print("The user can then choose to continue playing by typing yes or no, in lowercase, when prompted. ")
-print("The user can then choose to continue playing by typing yes or no, in lowercase, when prompted. If the user opts to continue playing, the game will start from the beginning.")
-print("If the user opts not to continue playing, the game will exit the loop.")
+print("After each series, you can choose to continue playing by typing yes or no, in lowercase.")
+
+while True:
+    while player_score < 3 and computer_score < 3:
+        computer = random.choice(['rock', 'paper', 'scissors'])
+        player = input("Enter your move (rock, paper, scissors): ").lower()
+
+        if player == computer:
+            print("It's a tie!")
+        elif (player == "rock" and computer == "scissors") or (player == "paper" and computer == "rock") or (player == "scissors" and computer == "paper"):
+            print("You win!")
+            player_score += 1
+        else:
+            print("You lose!")
+            computer_score += 1
+
+        print(f"Current score: You {player_score} - Computer {computer_score}")
+
