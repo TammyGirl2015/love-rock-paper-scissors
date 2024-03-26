32aa5256fab9b8bf336d38652da3c92d62e2b74b
TammyGirl2015 <mtamarie@yahoo.com>
2024-03-26 15:21:00 +0000

    add colorama library and color to code

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import random
+from colorama import Fore, Back, Style
 
 # Initialise scores
 player_score = 0
@@ -17,43 +18,53 @@ print("Type yes or no, in lowercase, to do this.")
 while True:
     while player_score < 3 and computer_score < 3:
         computer = random.choice(['rock', 'paper', 'scissors'])
-        player = input("Enter your move (rock, paper, scissors):\n ").lower()
+        player = input(Fore.BLUE + "Enter your move (rock, paper, scissors):\n ").lower()
+        print(Style.RESET_ALL)
 
         # Validate player's input
         while player not in ['rock', 'paper', 'scissors']:
-            print("Invalid choice. Please enter rock, paper, or scissors.")
+            print(Fore.RED + "Invalid choice. Please enter rock, paper, or scissors.")
+            print(Style.RESET_ALL)
             player = input("Enter your move (rock, paper, scissors): ").lower()
 
         if player == computer:
-            print("It's a tie!")
+            print(Fore.GREEN + "It's a tie!")
+            print(Style.RESET_ALL)
         elif (player == "rock" and computer == "scissors") or \
              (player == "paper" and computer == "rock") or \
              (player == "scissors" and computer == "paper"):
-            print("You win!")
+            print(Fore.GREEN + "You win!")
+            print(Style.RESET_ALL)
             player_score += 1
         else:
-            print("You lose!")
+            print(Fore.RED + "You lose!")
+            print(Style.RESET_ALL)
             computer_score += 1
 
         print(f"Current score: You {player_score} - Computer {computer_score}")
 
     # Determine the final winner of the series
     if player_score == 3:
-        print("Congratulations, you win the series!")
+        print(Fore.GREEN + "Congratulations, you win the series!")
+        print(Style.RESET_ALL)
     elif computer_score == 3:
         print("Computer wins the series!")
     else:
         print("The series is a draw!")
 
     # Ask the user if they want to continue playing
-    play_again = input("Play again? (yes/no):\n ").lower()
+    play_again = input(Fore.BLUE + "Play again? (yes/no):\n ").lower()
+    print(Style.RESET_ALL)
     while play_again not in ['yes', 'no']:
-        print("Invalid choice. Please enter yes or no.")
-        play_again = input("Play again? (yes/no): \n").lower()
+        print(Fore.RED + "Invalid choice. Please enter yes or no.")
+        print(Style.RESET_ALL)
+        play_again = input(Fore.BLUE + "Play again? (yes/no): \n").lower()
+        print(Style.RESET_ALL)
 
     if play_again == "yes":
         player_score = 0
         computer_score = 0
     else:
-        print("Thanks for playing!")
+        print(Fore.GREEN + "Thanks for playing!")
+        print(Style.RESET_ALL)
         break
