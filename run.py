7fc5f0b13ddb73a7043ed3d1a2cc73c4df587f0e
TammyGirl2015 <mtamarie@yahoo.com>
2024-03-27 02:58:03 +0000

    add welcome function and shorten long sentences in a better way

--- run.py
+++ run.py
@@ -5,38 +5,48 @@ from colorama import Fore, Back, Style
 player_score = 0
 computer_score = 0
 
-# Welcome message and instructions
-print(Fore.CYAN + "Welcome to rock, paper, scissors.")
-print("To play, enter your choice of rock, paper, or scissors in lowercase.")
-print("The computer will also choose one of the three choices.")
-print("Rock smashes scissors, scissors cuts paper, and paper covers rock.")
-print("The game series ends when the user or computer wins three rounds.")
-print("After each series, you can choose to continue playing.")
-print("Type yes or no, in lowercase, to do this.")
-print(Style.RESET_ALL)
+def welcome():
+    """
+    Welcome message and instructions
+    """
+    print(Fore.CYAN + "Welcome to rock, paper, scissors.")
+    print(
+        "To play, enter your choice of rock, paper, or scissors in lowercase."
+    )
+    print(
+        "The computer will also choose one of the three choices."
+    )
+    print(
+        "Rock smashes scissors, scissors cuts paper, and paper covers rock."
+    )
+    print(
+        "The game series ends when the user or computer wins three rounds."
+    )
+    print(
+        "After each series, you can choose to continue playing."
+    )
+    print(
+        "Type yes or no, in lowercase, to do this."
+    )
+    print(Style.RESET_ALL)
 
 # While loop that determines the winner of the round.
 while True:
     while player_score < 3 and computer_score < 3:
         computer = random.choice(['rock', 'paper', 'scissors'])
-        player = input(Fore.BLUE + "Enter your move (rock 🪨 , paper 🗒 , \
-scissors ✂ ):\n ").lower()
+        player = input(Fore.BLUE + "Enter your move (rock 🪨 , paper 🗒 , scissors ✂ ):\n ").lower()
         print(Style.RESET_ALL)
 
         # Validate player's input
         while player not in ['rock', 'paper', 'scissors']:
-            print(Fore.RED + "Invalid choice. Please enter rock 🪨 , paper 🗒 , \
-or scissors ✂ .")
+            print(Fore.RED + "Invalid choice. Please enter rock 🪨 , paper 🗒 , or scissors ✂ .")
             print(Style.RESET_ALL)
-            player = input("Enter your move (rock 🪨 , paper 🗒 , \
-scissors ✂ ): ").lower()
+            player = input("Enter your move (rock 🪨 , paper 🗒 , scissors ✂ ): ").lower()
 
         if player == computer:
             print(Fore.MAGENTA + "It's a tie.")
             print(Style.RESET_ALL)
-        elif (player == "rock" and computer == "scissors") or \
-             (player == "paper" and computer == "rock") or \
-             (player == "scissors" and computer == "paper"):
+        elif (player == "rock" and computer == "scissors") or (player == "paper" and computer == "rock") or (player == "scissors" and computer == "paper"):
             print(Fore.GREEN + "You win.")
             print(Style.RESET_ALL)
             player_score += 1
